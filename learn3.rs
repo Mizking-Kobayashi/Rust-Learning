c4b755dc09909d8c8c931c8725bf8f2d46c22f9d
Mizking-Kobayashi <kobayashi-m-ia-40hs@g.bears.ed.jp>
2025-07-22 00:08:16 +0900

    add: learn3.rs was added.

---
+++ learn3.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let num = 123456;
+    println!("number = {}", num);
+    println!("number = {:b}", num);
+    println!("number = {:o}", num);
+    println!("number = {:x}", num);
+    println!("number = {:X}", num);
+}
